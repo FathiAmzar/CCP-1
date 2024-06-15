4d4a3e10c3426822197eb6c71bd5a0521b12831b
FathiAmzar <143429862+FathiAmzar@users.noreply.github.com>
2024-06-16 03:27:16 +0800

    Add files via upload

---
+++ Assignment folder/main.cpp
@@ -0,0 +1,132 @@
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include "bonus_object.h"
+#include "race_track.h"
+
+using namespace std;
+
+int main() {
+    srand(time(0));
+
+    int enterpriseTrack = 0;
+    int voyagerTrack = 0;
+    int sonicTrack = 0;
+    int current_round = 1, rounds = 1, finish;
+
+    vector<BonusObject> bonuses;
+
+    cout << "***************************************************************************" << endl;
+    cout << "This game consist of 3 racers." << endl;
+    cout << "Player need to press enter to start a turn." << endl;
+    cout << "Player must put the bonus object on the track at the beginning of the game." << endl;
+    cout << "***************************************************************************" << endl;
+
+    cout << "\nEnter the number of the rounds: ";
+    cin >> rounds;
+    finish = rounds * 50;
+
+    cout << "Enter the number of bonus objects (2-6): ";
+    int bonusCount;
+    cin >> bonusCount;
+    if (bonusCount < 2) bonusCount = 2;
+    if (bonusCount > 6) bonusCount = 6;
+
+    for (int i = 0; i < bonusCount; ++i) {
+        int location;
+        int effect;
+        int value;
+
+        cout << "\nEnter location for bonus object " << i + 1 << " (1-49): ";
+        cin >> location;
+        while (location < 1 || location > 49) {
+            cout << "Invalid location. Enter location for bonus object " << i + 1 << " (1-49): ";
+            cin >> location;
+        }
+
+        cout << "Choose effect. \n1.forward \n2.backward \n3.double \n4.bonus turn\n";
+        cin >> effect;
+        while (effect < 1 || effect > 4) {
+            cout << "Invalid effect type. Choose effect. \n1. forward \n2. backward \n3. double \n4. bonus turn: ";
+            cin >> effect;
+        }
+
+        cout << "Enter value of the effect chosen: ";
+        cin >> value;
+
+        bonuses.push_back(BonusObject(location, effect, value));
+    }
+
+    cin.ignore(); // prevent race to start after entering round value
+    cout << "\nPress Enter to start the race\n";
+
+    while (true) {
+        cin.ignore();
+        cin.get(); // Wait for user to press Enter
+
+        // Generate random steps for Enterprise, Voyager, and Sonic
+        int enterpriseStep = getRandomSteps(1, 6);
+        int voyagerStep = getRandomSteps(2, 5);
+        int sonicStep = getRandomSteps(0, 7);
+
+        // Apply any collected bonus objects before moving
+        for (BonusObject& bonus : bonuses) {
+            if (enterpriseTrack % 50 == bonus.location) {
+                applyBonusEffect(enterpriseTrack, enterpriseStep, bonus.effect, bonus.value);
+            }
+            if (voyagerTrack % 50 == bonus.location) {
+                applyBonusEffect(voyagerTrack, voyagerStep, bonus.effect, bonus.value);
+            }
+            if (sonicTrack % 50 == bonus.location) {
+                applyBonusEffect(sonicTrack, sonicStep, bonus.effect, bonus.value);
+            }
+        }
+
+        // Update track positions
+        enterpriseTrack += enterpriseStep;
+        voyagerTrack += voyagerStep;
+        sonicTrack += sonicStep;
+
+        // Display track
+        display_track(enterpriseTrack, voyagerTrack, sonicTrack, rounds, 'e', 'v', 's');
+
+        // Display current track positions and steps taken
+        cout << "Current round: " << current_round << "/" << rounds << endl;
+        cout << "\n";
+        cout << "Enterprise current track: " << enterpriseTrack << endl;
+        cout << "Voyager current track: " << voyagerTrack << endl;
+        cout << "Sonic current track: " << sonicTrack << endl;
+        cout << "\n";
+        cout << "Enterprise step: " << enterpriseStep << endl;
+        cout << "Voyager step: " << voyagerStep << endl;
+        cout << "Sonic step: " << sonicStep << endl;
+
+        // Check if any racer has reached the finish line
+        if (enterpriseTrack >= finish && voyagerTrack >= finish && sonicTrack >= finish) {
+            cout << "It's a tie between all racers!" << endl;
+            break;
+        } else if (enterpriseTrack >= finish && voyagerTrack >= finish) {
+            cout << "It's a tie between Voyager and Enterprise!" << endl;
+            break;
+        } else if (enterpriseTrack >= finish && sonicTrack >= finish) {
+            cout << "It's a tie between Sonic and Enterprise!" << endl;
+            break;
+        } else if (voyagerTrack >= finish && sonicTrack >= finish) {
+            cout << "It's a tie between Voyager and Sonic" << endl;
+            break;
+        } else if (enterpriseTrack >= finish) {
+            cout << "Enterprise wins!" << endl;
+            break;
+        } else if (voyagerTrack >= finish) {
+            cout << "Voyager wins!" << endl;
+            break;
+        } else if (sonicTrack >= finish) {
+            cout << "Sonic wins!" << endl;
+            break;
+        } else {
+            cout << "\nPress Enter for the next turn\n";
+        }
+    }
+
+    return 0;
+}
---
+++ Assignment folder/race_track.cpp
@@ -0,0 +1,133 @@
+#include "race_track.h"
+#include <iostream>
+using namespace std;
+
+void display_track(int position1, int position2, int position3, int rounds, char character1, char character2, char character3) {
+    const int WIDTH = 57;
+    const int HEIGHT = 27;
+
+    char track[HEIGHT][WIDTH];
+
+    // initialize all to spaces
+    for (int i = 0; i < HEIGHT; i++)
+        for (int j = 0; j < WIDTH; j++)
+            track[i][j] = ' ';
+
+    // top track
+    for (int i = 0; i <= 2; i = i + 2) {
+        for (int j = 0; j < WIDTH - 1; j = j + 4) {
+            track[i][j] = '+';
+            track[i][j + 1] = '-';
+            track[i][j + 2] = '-';
+            track[i][j + 3] = '-';
+        }
+        track[i][WIDTH - 1] = '+';
+    }
+
+    for (int j = 0; j < WIDTH; j = j + 4)
+        track[1][j] = '|';
+
+    // left and right tracks - vertical lines
+    for (int i = 3; i < HEIGHT - 3; i = i + 2) {
+        track[i][0] = '|';
+        track[i][4] = '|';
+        track[i][WIDTH - 5] = '|';
+        track[i][WIDTH - 1] = '|';
+    }
+
+    // left track - horizontal lines
+    for (int i = 4; i < HEIGHT - 3; i = i + 2) {
+        track[i][0] = '+';
+        track[i][1] = '-';
+        track[i][2] = '-';
+        track[i][3] = '-';
+        track[i][4] = '+';
+    }
+
+    // right track - horizontal lines
+    for (int i = 4; i < HEIGHT - 3; i = i + 2) {
+        track[i][WIDTH - 5] = '+';
+        track[i][WIDTH - 4] = '-';
+        track[i][WIDTH - 3] = '-';
+        track[i][WIDTH - 2] = '-';
+        track[i][WIDTH - 1] = '+';
+    }
+
+    // bottom track
+    for (int i = HEIGHT - 3; i < HEIGHT; i = i + 2) {
+        for (int j = 0; j < WIDTH - 1; j = j + 4) {
+            track[i][j] = '+';
+            track[i][j + 1] = '-';
+            track[i][j + 2] = '-';
+            track[i][j + 3] = '-';
+        }
+        track[i][WIDTH - 1] = '+';
+    }
+
+    // initialize for next round
+    for (int j = 0; j < WIDTH; j = j + 4)
+        track[HEIGHT - 2][j] = '|';
+
+    while (position1 >= 50)
+        position1 -= 50;
+    while (position2 >= 50)
+        position2 -= 50;
+    while (position3 >= 50)
+        position3 -= 50;
+
+    // racer 1
+    if (position1 <= 13 && position1 >= 0) {
+        int x;
+        x = position1 * 4 + 1;
+        track[25][x] = character1;
+    } else if (position1 > 13 && position1 <= 25) {
+        int x = 25 - ((position1 - 13) * 2);
+        track[x][53] = character1;
+    } else if (position1 > 25 && position1 < 38) {
+        int x = 53 - (position1 - 25) * 4;
+        track[1][x] = character1;
+    } else if (position1 >= 38 && position1 < 50) {
+        int x = (position1 - 38) * 2 + 1;
+        track[x][1] = character1;
+    }
+
+    // racer 2
+    if (position2 <= 13 && position2 >= 0) {
+        int x;
+        x = position2 * 4 + 2;
+        track[25][x] = character2;
+    } else if (position2 > 13 && position2 <= 25) {
+        int x = 25 - ((position2 - 13) * 2);
+        track[x][54] = character2;
+    } else if (position2 > 25 && position2 < 38) {
+        int x = 54 - (position2 - 25) * 4;
+        track[1][x] = character2;
+    } else if (position2 >= 38 && position2 < 50) {
+        int x = (position2 - 38) * 2 + 1;
+        track[x][2] = character2;
+    }
+
+    // racer 3
+    if (position3 <= 13 && position3 >= 0) {
+        int x;
+        x = position3 * 4 + 3;
+        track[25][x] = character3;
+    } else if (position3 > 13 && position3 <= 25) {
+        int x = 25 - ((position3 - 13) * 2);
+        track[x][55] = character3;
+    } else if (position3 > 25 && position3 < 38) {
+        int x = 55 - (position3 - 25) * 4;
+        track[1][x] = character3;
+    } else if (position3 >= 38 && position3 < 50) {
+        int x = (position3 - 38) * 2 + 1;
+        track[x][3] = character3;
+    }
+
+    // display the track
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++)
+            cout << track[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
