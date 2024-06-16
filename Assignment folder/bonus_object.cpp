346029f8a1c573a7f432cd27b04edbb8906f9c2b
FathiAmzar <143429862+FathiAmzar@users.noreply.github.com>
2024-06-16 14:18:02 +0800

    Update bonus_object.cpp

--- Assignment folder/bonus_object.cpp
+++ Assignment folder/bonus_object.cpp
@@ -3,15 +3,3 @@
 int getRandomSteps(int min, int max) {
     return rand() % (max - min + 1) + min;
 }
-
-void applyBonusEffect(int& position, int& steps, const int& effect, int value) {
-    if (effect == 1) {
-        position += value;
-    } else if (effect == 2) {
-        position -= value;
-    } else if (effect == 3) {
-        steps *= 2;
-    } else if (effect == 4) {
-        steps += getRandomSteps(1, 10);
-    }
-}
