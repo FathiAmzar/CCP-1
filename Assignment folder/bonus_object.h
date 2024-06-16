3aea8ab3c1a680cb0c09a7eb0fdf6e86e5b539b4
FathiAmzar <143429862+FathiAmzar@users.noreply.github.com>
2024-06-16 14:18:17 +0800

    Update bonus_object.h

--- Assignment folder/bonus_object.h
+++ Assignment folder/bonus_object.h
@@ -8,6 +8,8 @@
 
 using namespace std;
 
+int getRandomSteps(int min, int max);
+
 class BonusObject {
 public:
     int location;
@@ -16,9 +18,25 @@ public:
 
     BonusObject() : location(0), effect(0), value(0) {}
     BonusObject(int loc, int eff, int val) : location(loc), effect(eff), value(val) {}
-};
 
-void applyBonusEffect(int& position, int& steps, const int& effect, int value);
-int getRandomSteps(int min, int max);
+    void applyBonusEffect(int& position, int& steps) const {
+        switch (effect) {
+            case 1: // Forward
+                position += value;
+                break;
+            case 2: // Backward
+                position -= value;
+                break;
+            case 3: // Double steps
+                steps *= 2;
+                break;
+            case 4: // Extra turn
+                steps += getRandomSteps(1, 9);
+                break;
+            default:
+                break;
+        }
+    }
+};
 
 #endif
