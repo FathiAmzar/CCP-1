c9dc5f62e96304943a97610928f3274991ec99fc
FathiAmzar <143429862+FathiAmzar@users.noreply.github.com>
2024-06-16 13:19:06 +0800

    Update race_track.h

--- Assignment folder/race_track.h
+++ Assignment folder/race_track.h
@@ -4,5 +4,6 @@
 #include <iostream>
 
 void display_track(int position1, int position2, int position3, int rounds, char symbol1, char symbol2, char symbol3);
+int update_current_round(int a, int b, int c);
 
 #endif
